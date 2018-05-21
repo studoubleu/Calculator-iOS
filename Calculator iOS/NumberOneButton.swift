a03f8dfe0f6d5f28d86ee870375a9204e784e593
Stuart Wells <studoubleu@me.com>
2018-05-21 00:21:20 -0600

    Added IBActions
    Added functionality to numeric buttons

---
+++ Calculator iOS/NumberOneButton.swift
@@ -0,0 +1,9 @@
+//
+//  NumberOneButton.swift
+//  Calculator iOS
+//
+//  Created by Stuart Wells on 5/20/18.
+//  Copyright © 2018 Stuart Wells. All rights reserved.
+//
+
+import Foundation
--- Calculator iOS/ViewController.swift
+++ Calculator iOS/ViewController.swift
@@ -19,7 +19,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func Button1(sender: UIButton) {
+        print("1")
+    }
+    @IBAction func Button2(sender: UIButton) {
+        print("2")
+    }
+    @IBAction func Button3(sender: UIButton) {
+        print("3")
+    }
+    @IBAction func Button4(sender: UIButton) {
+        print("4")
+    }
+    @IBAction func Button5(sender: UIButton) {
+        print("5")
+    }
+    @IBAction func Button6(sender: UIButton) {
+        print("6")
+    }
+    @IBAction func Button7(sender: UIButton) {
+        print("7")
+    }
+    @IBAction func Button8(sender: UIButton) {
+        print("8")
+    }
+    @IBAction func Button9(sender: UIButton) {
+        print("9")
+    }
+    @IBAction func Button0(sender: UIButton) {
+        print("0")
+    }
+ 
 }
 
